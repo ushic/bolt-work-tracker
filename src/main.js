30c111a62afe629630055434dd3ad75e02bc33cd
ushic <ushic@yahoo.com>
2025-01-08 10:44:54 +0330

    Delete src directory

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import './i18n';
-import './styles/globals.css';
-import { createSidebar } from './src/components/Navigation/Sidebar';
-import { initializeApp } from './src/app';
-import { setupLanguageObserver } from './utils/languageObserver';
-
-const app = document.querySelector('#app');
-initializeApp(app);
-setupLanguageObserver();
